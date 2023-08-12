bd22ca66016f329b44e6dcaae4b99d0224ea3479
Nick Thacke <ncthacke@gmail.com>
2023-08-12 15:37:08 -0400

    Login Acocunt Service inhibits behavior of LoginAccountDAO

---
+++ src/main/java/com/example/nick/thacke/Movie/Ratings/service/create_account/LoginAccountService.java
@@ -0,0 +1,10 @@
+package com.example.nick.thacke.Movie.Ratings.service.create_account;
+
+import com.example.nick.thacke.Movie.Ratings.dao.create_account.LoginAccountDAO;
+import com.example.nick.thacke.Movie.Ratings.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginAccountService implements LoginAccountDAO {
+
+}
